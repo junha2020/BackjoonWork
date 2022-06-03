8e6e174ad95eaa29ac95a9536d74a8b60912b9cb
junha2020 <junha2020@naver.com>
2022-06-03 21:40:38 +0900

    BaekjoonWhile

--- while/src/cycle.java
+++ while/src/cycle.java
@@ -1,19 +1,24 @@
-import java.io.*;
+import java.util.*;
 
 public class cycle {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-        int b = a;
-        int c = 1;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int fnum = sc.nextInt();
+        int num = fnum;
+        int nnum, tnum, onum, to;
+        int c = 0;
 
-        while (true) {
-            b = ((a%10)*10)+ (((a/10)+(a%10))%10);
-            c++;
-            if (b == a) {
-                break;
+        do {
+            tnum = num / 10;
+            onum = num % 10;
+            to = tnum + onum;
+            if (to >= 10) {
+                to = to - 10;
             }
-        }
+            nnum = onum * 10 + to;
+            num = nnum;
+            c++;
+        } while (fnum != nnum);
         System.out.println(c);
     }
 }
