0b1adb61d004ca1c55dab84cc7d1a363fc831170
junha2020 <junha2020@naver.com>
2022-06-03 21:40:07 +0900

    BaekjoonFunction

---
+++ Function/src/sumn.java
@@ -0,0 +1,16 @@
+class Test {
+    long sum(int[] a) {
+        int s = 0;
+        for (int i = 0; i < a.length; i++) {
+            s += a[i];
+        }
+        return s;
+    }
+}
+
+public class sumn {
+    public static void main(String[] args) {
+        Test t = new Test();
+
+    }
+}
