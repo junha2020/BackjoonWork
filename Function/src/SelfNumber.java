5c7d127c168a45cc72ce6034961fad14ef1000c6
junha2020 <junha2020@naver.com>
2022-06-05 17:59:02 +0900

    BaekjoonFunction

---
+++ Function/src/SelfNumber.java
@@ -0,0 +1,10 @@
+public class SelfNumber {
+    int show() {
+        for (int i = 0; i <= 10000; i++) {
+            int
+        }
+    }
+    public static void main(String[] args) {
+        SelfNumber sf = new SelfNumber();
+    }
+}
